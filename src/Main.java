b5f02f4b833af6310f235fe2f06e64c70ded814b
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-07-04 20:22:11 -0700

    Create Cupcake class

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    // Create a new class named Cupcake.
+    // This will be the class all other cupcakes inherit from
+    class Cupcake {
+        public double price;
+
+        public double getPrice() {
+            return this.price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        public void type() {
+            System.out.println("A basic, generic cupcake, with vanilla frosting");
+        }
+    }
 }
